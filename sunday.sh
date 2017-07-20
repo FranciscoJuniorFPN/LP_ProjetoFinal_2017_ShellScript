736c15d713be693103f45f5c1266a4e64405a84c
JosemarRocha <josemarrochads@gmail.com>
2017-07-19 20:06:46 -0400

    PROJETO FINAL, VERSÃO FINAL DO CÓDIGO!
    Versão final do código do programa para o trabalho do projeto final de linguagens de programação

--- sunday.sh
+++ sunday.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
 
 function firstroutine {
- 	Url=$(whiptail --title "Entrada de url pelo usuário" --inputbox "Digite a url:" --fb 10 60 3>&1 1>&2 2>&3)
+	Url=$(whiptail --title "Entrada de url pelo usuário" --inputbox "Digite a url:" --fb 10 60 3>&1 1>&2 2>&3)
 
-	firefox $Url  
+	firefox $Url &
+
 }
 
 function secondroutine {
-	now=$(date +"%T")
-	zenity --info --title="Horário" --text "tempo atual : $now" --width="100" height="50" 2> /dev/null
+	rhythmbox-client --play @
 }
 
 
 function thirdroutine {
-	 rhythmbox-client --play @
+	now=$(date +"%T")
+	zenity --info --title="Horário" --text "tempo atual : $now" --width="100" height="50" 2> /dev/null
 }
 
 whiptail --title "Sunday Mk.IV" --checklist --separate-output "Escolha:" 15 60 4 \
-"Mozilla" "" on \
-"Horario" "" off \
-"Rhythmbox" "" off 2>results
+"Mozilla" "" OFF \
+"Rhythmbox" "" OFF \
+"Horario" "" OFF 2>results
 
 while read choice
 do
         case $choice in
                 Mozilla) firstroutine
                 ;;
-                Horario) secondroutine
+                Rhythmbox) secondroutine
                 ;;
-                Rhythmbox) thirdroutine
+                Horario) thirdroutine
                 ;;
                 *)
                 ;;
         esac
 done < results
+
